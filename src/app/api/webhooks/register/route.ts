b5ca95a2e063033059803263e4d5a191b4c3c707
yogendrasinh jadeja <meyogendrasinh.jadeja@gmail.com>
2025-01-25 08:34:50 +0530

    clerk code

--- src/app/api/webhooks/register/route.ts
+++ src/app/api/webhooks/register/route.ts
@@ -11,18 +11,15 @@ export async function POST(req: NextRequest) {
     }
 
     const headerPayload = await headers()
-
-    const svixId = headerPayload.get("svix-id");
-    const svixTimestamp = headerPayload.get("svix-timestamp");
-    const svixSignature = headerPayload.get("svix-signature");
-
-    if (!svixId || !svixTimestamp || !svixSignature) {
-        return NextResponse.json(
-            { error: "Missing required Svix headers.", Data:{
-                svixId,svixTimestamp,svixSignature
-            } },
-            { status: 400 }
-        );
+    const svix_id = headerPayload.get('svix-id')
+    const svix_timestamp = headerPayload.get('svix-timestamp')
+    const svix_signature = headerPayload.get('svix-signature')
+  
+    // If there are no headers, error out
+    if (!svix_id || !svix_timestamp || !svix_signature) {
+      return new Response('Error: Missing Svix headers', {
+        status: 400,
+      })
     }
 
     const payload = await req.json()
@@ -34,9 +31,9 @@ export async function POST(req: NextRequest) {
 
     try {
         evt = wh.verify(body, {
-            "svix-id": svixId,
-            "svix-timestamp": svixTimestamp,
-            "svix-signature": svixSignature
+            "svix-id": svix_id,
+            "svix-timestamp": svix_timestamp,
+            "svix-signature": svix_signature
         }) as WebhookEvent
     } catch (error) {
         console.error("Error verifying webhook")
